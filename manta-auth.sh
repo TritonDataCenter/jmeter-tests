2b41b53fc38ec1daf88a01b39a7179dba8226f72
3388fb3c78c6a82b5e0137aa986d2453e72af49c <douglas.andeson@joyent.com>
2019-07-17 11:53:40 -0500

    Updating the buckets test.

--- manta-auth.sh
+++ manta-auth.sh
@@ -5,13 +5,14 @@ alg=rsa-sha256
 if [ -z ${KEY_LOCATION} ];
 then 
   KEY_LOCATION=$HOME/.ssh/id_rsa.pub;
+  P_KEY=$HOME/.ssh/id_rsa 
 fi
 MANTA_KEY_ID=$(ssh-keygen -E md5 -lf $KEY_LOCATION | sed 's/MD5://' | awk '{print $2}')
 keyId=/$MANTA_USER/keys/$MANTA_KEY_ID
 now=$(date -u "+%a, %d %h %Y %H:%M:%S GMT")
 sig=$(echo "date:" $now | \
 		    tr -d '\n' | \
-                    openssl dgst -sha256 -sign $HOME/.ssh/id_rsa | \
+                    openssl dgst -sha256 -sign ${P_KEY} | \
                     openssl enc -e -a | tr -d '\n')
-printf "{ \n    \"alg\" : \"$alg\",\n    \"key\" : \"${keyId}\" ,\n    \"sig\" : \"${sig}\",\n    \"date\" : \"${now}\" }\n";
+printf "{ \n    \"alg\" : \"$alg\",\n    \"key\" : \"${keyId}\" ,\n    \"sig\" : \"${sig}\",\n    \"date\" : \"${now}\" }\n"
        
